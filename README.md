7b5b1d72021afe7dc34476d5366d41e8b602f5de
冯文议 <xfsy_2015@163.com>
2019-11-16 00:25:11 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,21 @@
+
+## 模块
+
+注册中心
+配置中心
+网关
+模块
+    - 模块1
+    - 模块2
+    - 接口
+
+```
+study-springcloud
+├── README.md
+├── pom.xml
+├── springcloud-config
+├── springcloud-eureka
+├── springcloud-gateway
+├── springcloud-modules
+└── study-springcloud.iml
+```
---
+++ springcloud-eureka/src/main/java/com/fengwenyi/springcloud_eureka/SpringCloudEurekaApplication.java
@@ -0,0 +1,19 @@
+package com.fengwenyi.springcloud_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Erwin Feng<xfsy_2015@163.com>
+ * @since 2019/11/16 00:17
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaApplication.class, args);
+    }
+
+}
