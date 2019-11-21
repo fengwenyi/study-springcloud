51cbd498b81e98e47482feb97772f4160a2703d5
冯文议 <xfsy_2015@163.com>
2019-11-21 16:02:09 +0800

    创建订单模块

---
+++ springcloud-modules/springcloud-module-order/src/main/java/com/fengwenyi/springcloud_module_order/SpringCloudModuleOrderApplication.java
@@ -0,0 +1,17 @@
+package com.fengwenyi.springcloud_module_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * @author Erwin Feng[xfsy_2015@163.com]
+ * @since 2019/11/21 15:59
+ */
+@SpringCloudApplication
+public class SpringCloudModuleOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudModuleOrderApplication.class, args);
+    }
+
+}
